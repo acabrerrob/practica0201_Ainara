e87ef425c64436f97a5fae358b922e5b8b6714db
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-16 19:57:10 +0200

    Update Ejercicio2.py

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,7 @@
-name= input('Introduzca su nombre completo')
-nuevasstr=''
-print(name.lower())
-print(name.upper())
+nombre= input('Introduzca su nombre completo')
+nuevastr=''
+
+print(nombre.lower())
+print(nombre.upper())
+
+splitnombre=nombre.split() #separo cada letra
