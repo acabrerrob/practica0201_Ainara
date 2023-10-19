32c9d05a6ffdf3c23c3214ddc88b65e975a90726
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-19 16:42:02 +0200

    Update Ejercicio10.py

--- Ejercicio10.py
+++ Ejercicio10.py
@@ -1,4 +1,3 @@
-#pedri productos de compra separados por comas , imprimirlos en lineas distitas
 list = input('Introduzca la lista de la comrpa separando cada artículo por comas')
 
 
@@ -6,5 +5,3 @@ splitlist = list.split(',')
 
 for list in  splitlist:
     print(list)
-else :
-    print('sos un gil')
