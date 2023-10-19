88f891d85d4b32656d7ae86b9a3b1cabda96175c
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-19 16:30:53 +0200

    Update Ejercicio10.py

---
+++ Ejercicio10.py
@@ -0,0 +1,6 @@
+#pedri productos de compra separados por comas , imprimirlos en lineas distitas
+shoppingL = input('Introduzca la lista de la comrpa separando cada artículo por comas')
+
+splitshoppingL = shoppingL.split(',')
+
+print(splitshoppingL+(\n))
