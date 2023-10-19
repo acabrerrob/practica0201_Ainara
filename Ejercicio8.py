dfb52f6f360fcfeca37569c8f394e5e031b70591
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-19 16:02:11 +0200

    Update Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,5 @@
+price = input ('Introduzca el preio en euros con 2 decimales ')
+
+splitprice = price.split('.')
+
+print('Usted ha introducido ', splitprice[0], 'euros y ', splitprice[1], 'céntimos')
