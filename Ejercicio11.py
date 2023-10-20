db89b2c94b9e460e5e432979d2569f8a6fe2e725
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-20 23:33:17 +0200

    Update Ejercicio11.py

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,6 +1,14 @@
-#pedir nombre de producto, precio del mismo y cantidad de unidades
 name = input('Introduzca el nombre del artículo ')
 price = float (input ('Introduzca el precio del producto '))
 units = int (input ('Introduzca el número de unidades '))
 
-print( name, )
+totalPrice = price * units
+
+formatPrice = "{:010,.2f}".format(price)
+formatUnits = "{:03d}".format(units)
+formatTotalPrice = "{:,.2f}".format(totalPrice)
+
+print('El nombre del producto es: ', name)
+print('El precio unitario es de: ', formatPrice, '€')
+print('El número de unidades: ', formatUnits, 'uds')
+print('El coste total es de: ', formatTotalPrice, '€')
