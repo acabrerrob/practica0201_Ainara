cd57245a93121808d492c1e79bb11011fd2b9f39
acabrerrob <acabrerrob@educacion.navarra.es>
2023-10-20 17:23:54 +0200

    Update Ejercicio11.py

---
+++ Ejercicio11.py
@@ -0,0 +1,4 @@
+#pedir nombre de producto, precio del mismo y cantidad de unidades
+product = input('Introduzca el nombre del artículo')
+price = float (input('Introduzca el precio del producto'))
+units = int (input('Introduzca el número de unidades'))
